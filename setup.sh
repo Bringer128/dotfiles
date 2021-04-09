d2ac70317ca0378d8577d886870b7761efccdf55
Ryan Musgrave <ryan@Ryans-MacBook-Pro.local>
2021-04-09 09:35:28 -0400

    Try out projector

--- setup.sh
+++ setup.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
+
+if [[ -z ${SPIN} ]]; then
+	echo 'Must be run within a Spin environment' >&2
+	exit 1
+fi
+
+startProjector() {
+	local idePattern=${1}
+	sudo apt-get install -qq --no-install-recommends \
+		libfreetype6 \
+		libext6 \
+		libxi6 \
+		libxrender1 \
+		libxtst6 \
+		python3-dev \
+		python3-pip
+
+	pip3 install --quiert --user projector-installer
+	mkdir -p ~/.projector/configs
+	projector ~/.local/bin/projector ide install "${idePattern}" &
+}
+
+
+startProjector 'RubyMine'
+
 cp gitconfig ~/.gitconfig
